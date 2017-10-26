95ef313896d9c3737abf298e74ca5fd6639dcf85
Raghavendra SC <raghavendra_sc@intuit.com>
2017-10-26 23:41:34 +0530

    Added Error

--- src/main/java/com/tictactoe/TicTacToeController.java
+++ src/main/java/com/tictactoe/TicTacToeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tictactoe.bl.TicTacToeService;
+import com.tictactoe.bl.Error;
 
 @RestController
 public class TicTacToeController {
@@ -20,24 +21,30 @@ public class TicTacToeController {
     public String newGame() throws JsonGenerationException, JsonMappingException, IOException {
     	if(ticTacToeService.getStatus() == null) {
     		return getResponseString(ticTacToeService.getNewGame());
+    	} else {
+    		Error err = new Error("Game is Inprogress, Cannot create a new Game");
+    		return getResponseString(err);
     	}
-        return null;
     }
 
     @RequestMapping("/play")
     public String playMove(@RequestParam(value="player") String player, @RequestParam(value="x") String x, @RequestParam(value="y") String y) throws JsonGenerationException, JsonMappingException, NumberFormatException, IOException {
     	if(ticTacToeService.getStatus() != null) {
    		 	return getResponseString(ticTacToeService.playMove(Player.getPlayer(Integer.parseInt(player)), Integer.parseInt(x), Integer.parseInt(y)));
+    	} else {
+    		Error err = new Error("No Game In progress");
+    		return getResponseString(err);
     	}
-    	return null;
     }
     
     @RequestMapping("/status")
     public String getStatus() throws JsonGenerationException, JsonMappingException, IOException {
     	if(ticTacToeService.getStatus() != null) {
     		 return getResponseString(ticTacToeService.getStatus());
+    	} else {
+    		Error err = new Error("No Game In progress, Create a new game");
+    		return getResponseString(err);
     	}
-        return null;
     }
     
     private String getResponseString(Object obj) throws JsonGenerationException, JsonMappingException, IOException {
---
+++ src/main/java/com/tictactoe/bl/Error.java
@@ -0,0 +1,18 @@
+package com.tictactoe.bl;
+
+public class Error {
+	String errorMessage = null;
+	
+	public Error(String msg) {
+		errorMessage = msg;
+	}
+
+	public String getMessage() {
+		return errorMessage;
+	}
+
+	public void setMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+  
+}
